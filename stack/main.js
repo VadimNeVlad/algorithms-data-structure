bd92e62ff92d4c29dae40611ac2e959534f41e5b
Vadym Marukhnenko <vdm.marukhnenko@gmail.com>
2023-10-10 16:27:41 +0100

    implemented Stack

---
+++ stack/main.js
@@ -0,0 +1,30 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (!this.items.length) return "items are empty";
+    return this.items.pop();
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+}
